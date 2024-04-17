5794015980dcdc17cbe3cab0634f0c71245ecb4b
JavascriptJerry <153471821+JavascriptJerry@users.noreply.github.com>
2024-04-17 15:59:28 -0700

    Add files via upload
    project

---
+++ defmain.py
@@ -0,0 +1,23 @@
+import time
+
+
+def main():
+    lal1 = input("What is your  first name -")
+    time.sleep(1.5)
+    lal2 = input("What is your last name? -")
+    time.sleep(2.5)
+    print("  ")
+    time.sleep(2.5)
+    print(f"Never gonna give {lal1} up")
+    time.sleep(2.5)
+    print(f"Never gonna give {lal1} up everrrrie {lal2} is never gonna desert you especcialy not his")
+    time.sleep(2.5)
+    print(f"{lal2} up his {lal1} is his precious evverie YEAH BABY!")
+    time.sleep(2.5)
+    print(f"{lal1} is going up the stairs with his friend {lal2}")
+    time.sleep(2.5)
+    print(f"{lal2} up his his his his {lal1}")
+    time.sleep(1)
+main()
+time.sleep(100)
+
